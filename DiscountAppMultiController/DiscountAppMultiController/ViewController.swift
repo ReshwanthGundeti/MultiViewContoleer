15b00d71da9645894978db9b67287c36703c8968
ReshwanthGundeti <89552030+ReshwanthGundeti@users.noreply.github.com>
2022-03-22 12:03:23 -0500

    Add files via upload

---
+++ DiscountAppMultiController/DiscountAppMultiController/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  DiscountAppMultiController
+//
+//  Created by Gundeti,Reshwanth on 3/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var amountOutlet: UITextField!
+    
+    
+    @IBOutlet weak var discountRateOutlet: UITextField!
+    
+    var priceAfterDiscount =  0.0
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func calcDiscountButtonClicked(_ sender: Any) {
+        
+        
+        //Read the values and convert to Double
+        
+        var amount = Double(amountOutlet.text!)
+        
+        var discountRate = Double(discountRateOutlet.text!)
+        
+        
+        
+       priceAfterDiscount = amount! - (amount!*discountRate!/100)
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        var transition = segue.identifier
+        if transition  == "resultSegue"
+        {
+            //MAke the result view controller as destination
+            var destination = segue.destination as! ResultViewController
+            
+            destination.amount = amountOutlet.text!
+            destination.discrate = discountRateOutlet.text!
+            destination.priceAfterDisc = String(priceAfterDiscount)
+        }
+    }
+    
+
+
+}
+
